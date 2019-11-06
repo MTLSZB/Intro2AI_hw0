1718e6b5944f70c70cb12e0ba88f3d1e9d50238e
מיטל ברכה <m.szabo@campus.technion.ac.il>
2019-11-06 17:48:51 +0200

    Initial commit

---
+++ helloWorld.py
@@ -0,0 +1,19 @@
+def boolean(bool):
+    if(bool):
+        return 1
+    return -1
+
+
+def power(x,y):
+    return x**y
+
+
+if __name__ == "__main__":
+    print('Hello World!')
+    print(boolean(True), boolean(False))
+    print(power(2,10), power(3,5))
+    
+
+
+
+
